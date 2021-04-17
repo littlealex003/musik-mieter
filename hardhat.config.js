07956d106d16fd2bb3d1bc3528153bbb7434f139
littlealex003 <littlealex003@gmail.com>
2021-04-17 18:39:59 +0200

    added test networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
+
 module.exports = {
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${alchemyApiKey}`,
+      accounts: {mnemonic: mnemonic}
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+      accounts: {mnemonic: mnemonic}
+    }
+  },
   solidity: "0.7.3",
 };
